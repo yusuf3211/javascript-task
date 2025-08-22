e7f60f65f636183e7134ee9ff27d969df570b99d
yusuf3211 <wellplayed3211@gmail.com>
2025-08-23 02:23:22 +0700

    training3

---
+++ train3.js
@@ -0,0 +1,32 @@
+
+const data = {
+    id          : 1,
+    name        : "leanne Graham",
+    username    : "Bret",
+    email       : "Sincere@april.biz",
+    address     :
+    {
+        street  : "kulkas Light",
+        suite   : "apt. 556",
+        city    : "Gwenborough",
+        zipcode : "92998-3874",
+    },
+    phone       : "1-770-736-8031 x56442",
+    website     : "hildegard.org",
+}
+
+
+
+console.log("id      :",data.id)
+console.log("name    :",data.name)
+console.log("username:",data.username)
+console.log("email   :",data.email)
+console.log()
+console.log("address :")
+console.log("         street  :",data.address.street)
+console.log("         suite   :",data.address.suite)
+console.log("         city    :",data.address.city)
+console.log("         zipcode :",data.address.zipcode)
+console.log()
+console.log("phone   :",data.phone)
+console.log("website :",data.website)
